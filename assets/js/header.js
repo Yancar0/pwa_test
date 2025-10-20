24200fc4d5c96fb786bf3e466a7c70450e1fc6a0
Yancar0 <7056juancarlossanchezgalicia@gmail.com>
2025-10-20 12:20:41 -0600

    second commit

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,5 +1,5 @@
 const BASE_PATH = window.location.hostname.includes("github.io")
-  ? "/pwa_test" // 👈 nombre exacto de tu repositorio
+  ? "/pwa_test" // nombre exacto de tu repositorio
   : ""; // en local (XAMPP)
 
 export async function loadHeader() {
@@ -14,24 +14,4 @@ export async function loadFooter() {
   footer.innerHTML = await response.text();
 }
 
-/*export async function loadHeader() {
-  const res = await fetch("../../partials/header.html"); // 🔧 ruta relativa al proyecto
-  const html = await res.text();
-  document.getElementById("header").innerHTML = html;
-}
 
-export async function loadFooter() {
-  const res = await fetch("../../partials/footer.html"); // 🔧 igual aquí
-  const html = await res.text();
-  document.getElementById("footer").innerHTML = html;
-}*/
-
-/*export async function loadHeader() {
-  const res = await fetch ("../../partials/header.html");
-  document.getElementById("header").innerHTML = await res.text();
-}
-
-export async function loadFooter() {
-  const res = await fetch ("../../partials/footer.html");
-  document.getElementById("footer").innerHTML = await res.text();
-}*/
--- sw.js
+++ sw.js
@@ -1,11 +1,15 @@
-const CACHE_NAME = "pwa2-cache-v1";
+const CACHE_NAME = "pwa2-cache-v3";
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
+  "./manifest.json",
+  "./sw.js",
   "./assets/css/style.css",
+  "./assets/js/header.js",
   "./assets/js/main.js",
   "./partials/header.html",
   "./partials/footer.html",
+  "./temas/potencias/potencias-base10.html",  
 ];
 
 self.addEventListener("install", (event) => {
@@ -36,58 +40,3 @@ self.addEventListener("activate", (event) => {
     )
   );
 });
-
-/*const cacheName = "v1";
-
-//navigator.serviceWorker.register("./sw.js");
-
-// Evento INSTALL → guarda en caché
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(cacheName).then((cache) => {
-      return cache.addAll([
-        // Archivos a cachear durante la instalación
-        "/",
-        "./index.html",
-        "./partials/header.html",
-        "./partials/footer.html",
-        "./assets/css/style.css",
-        "./assets/js/main.js",
-        "./assets/js/header.js",
-        "./temas/potencias/potencias-base10.html",
-      ]);
-    })
-  );
-});
-
-
-// Evento ACTIVATE → limpia caches viejas
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter((key) => key !== cacheName).map((key) => caches.delete(key))
-      );
-    })
-  );
-});
-
-// Evento FETCH → estrategia cache first con fallback
-self.addEventListener("fetch", (event) => {
-  console.log("Interceptando:", event.request.url);
-
-  // Caso especial: cualquier .jpg responde con unicorn.jpg
-  //if (/\.jpg$/.test(event.request.url)) {
-  // event.respondWith(caches.match("unicorn.jpg"));
-  //return;
-  //}
-
-  // Para lo demás: intenta caché primero, luego red
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
-*/
-
